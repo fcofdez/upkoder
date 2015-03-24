0c690b353d0a1f3cb18f7432a892385c9758200d
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2015-03-24 14:10:54 +0100

    Adding whole process related to ffmpeg.

--- src/main/scala/worker/WorkExecutor.scala
+++ src/main/scala/worker/WorkExecutor.scala
@@ -1,30 +1,41 @@
 package worker
 
 import akka.actor.Actor
+import java.io.File
+import java.net.URL
+import sys.process._
 import util.Random.nextInt
+
 
 class WorkExecutor extends Actor {
   def receive = {
     case url: String =>
       val filename = download_video(url)
-      val x = encode(filename)
+      val x = generateThumbnails(filename)
   }
 
   def getDuration(filePath: String): Int = {
-    import sys.process._
     val info = Seq("ffprobe", "-i",  filePath, "-show_format", "-loglevel", "quiet")
     info.lines.filter(_.contains("duration")).map(_.replace("duration=", "")).mkString.toDouble.toInt
   }
 
-  def generateThumbnails(filePath: String) Seq[String] = {
-    Seq.fill(3)(nextInt(getDuration(filePath)))
-    val command = Seq("ffmpeg", "-i", filePath, "-deinterlace", "-an", "-ss", second, "-t", "00:00:01", "-r" "1", "-y", "-vcodec", "mjpeg", "-f", "mjpeg", "-loglevel", "quiet" target)
+  def generateThumbnail(filePath: String, second: Int): String = {
+    val outputFilePath = File.createTempFile("thumbnail-", ".jpg").getPath()
+    val command = Seq("ffmpeg", "-i", filePath, "-deinterlace", "-an", "-ss", second.toString, "-t", "00:00:01", "-r", "1", "-y", "-vcodec", "mjpeg", "-f", "mjpeg", "-loglevel", "quiet", outputFilePath).!
+    outputFilePath
+  }
+
+  def encode(filePath: String): String = {
+    val outputFilePath = File.createTempFile("video-", ".mp4").getPath()
+    Seq("ffmpeg", "-i", filePath, "-strict", "experimental", "-codec:a", "aac", "-b:a", "64k", "-b:v", "1000000", outputFilePath, "-y", "-loglevel", "quiet").!
+    outputFilePath
+  }
+
+  def generateThumbnails(filePath: String): Seq[String] = {
+    Seq.fill(3)(nextInt(getDuration(filePath))).map{ second => generateThumbnail(filePath, second)}
   }
 
   def download_video(url: String): String = {
-    import sys.process._
-    import java.net.URL
-    import java.io.File
     val f = File.createTempFile("video", ".mp4")
     val filename = f.getPath()
     (new URL(url) #> f !!)
