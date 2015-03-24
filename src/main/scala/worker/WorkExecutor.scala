ef833c2575aab2ea426a54a94a013dfecde45ccd
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2015-03-24 13:31:05 +0100

    More work.

--- src/main/scala/worker/WorkExecutor.scala
+++ src/main/scala/worker/WorkExecutor.scala
@@ -1,11 +1,24 @@
 package worker
 
 import akka.actor.Actor
+import util.Random.nextInt
 
 class WorkExecutor extends Actor {
   def receive = {
     case url: String =>
-      download_video(url)
+      val filename = download_video(url)
+      val x = encode(filename)
+  }
+
+  def getDuration(filePath: String): Int = {
+    import sys.process._
+    val info = Seq("ffprobe", "-i",  filePath, "-show_format", "-loglevel", "quiet")
+    info.lines.filter(_.contains("duration")).map(_.replace("duration=", "")).mkString.toDouble.toInt
+  }
+
+  def generateThumbnails(filePath: String) Seq[String] = {
+    Seq.fill(3)(nextInt(getDuration(filePath)))
+    val command = Seq("ffmpeg", "-i", filePath, "-deinterlace", "-an", "-ss", second, "-t", "00:00:01", "-r" "1", "-y", "-vcodec", "mjpeg", "-f", "mjpeg", "-loglevel", "quiet" target)
   }
 
   def download_video(url: String): String = {
