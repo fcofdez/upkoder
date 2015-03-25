5934f7b6727821dfd40c658b408c901e30519079
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2015-03-25 19:28:49 +0100

    Work in progress with frontend actor.

--- build.sbt
+++ build.sbt
@@ -5,15 +5,21 @@ version := "0.1"
 scalaVersion := "2.11.6"
 
 
-libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-contrib" % "2.3.9",
-  "com.typesafe.akka" %% "akka-testkit" % "2.3.9",
-  "com.typesafe.akka" % "akka-http-experimental_2.11" % "1.0-M4",
-  "com.typesafe.akka" % "akka-http-core-experimental_2.11" % "1.0-M4",
-  "com.typesafe.akka" % "akka-stream-experimental_2.11" % "1.0-M4",
+libraryDependencies ++= {
+  val akkaStreamV = "1.0-M3"
+  Seq(
+  "com.typesafe.akka"  %% "akka-contrib" % "2.3.9",
+  "com.typesafe.akka"  %% "akka-testkit" % "2.3.9",
+  "com.typesafe.akka"  %% "akka-stream-experimental"          % akkaStreamV,
+  "com.typesafe.akka"  %% "akka-http-core-experimental"       % akkaStreamV,
+  "com.typesafe.akka"  %% "akka-http-experimental"            % akkaStreamV,
+  "com.typesafe.akka"  %% "akka-http-spray-json-experimental" % akkaStreamV,
+  "com.typesafe.akka"  %% "akka-http-testkit-experimental"    % akkaStreamV,
   "com.github.seratch" %% "awscala" % "0.5.+",
-  "org.scalatest" %% "scalatest" % "2.2.1" % "test",
-  "commons-io" % "commons-io" % "2.4" % "test")
+  "org.scalatest"      %% "scalatest" % "2.2.1" % "test",
+  "commons-io"         % "commons-io" % "2.4" % "test"
+  )
+}
 
 
 fork in run := true
--- src/main/scala/worker/Main.scala
+++ src/main/scala/worker/Main.scala
@@ -1,7 +1,60 @@
-package worker
+import akka.actor.ActorSystem
+import akka.event.{LoggingAdapter, Logging}
+import akka.http.Http
+import akka.http.client.RequestBuilding
+import akka.http.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.marshalling.ToResponseMarshallable
+import akka.http.model.{HttpResponse, HttpRequest}
+import akka.http.model.StatusCodes._
+import akka.http.server.Directives._
+import akka.http.unmarshalling.Unmarshal
+import akka.stream.{ActorFlowMaterializer, FlowMaterializer}
+import akka.stream.scaladsl.{Sink, Source}
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import java.io.IOException
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.math._
+import spray.json.DefaultJsonProtocol
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello, worla!")
+case class IpInfo(ip: String, country: Option[String], city: Option[String], latitude: Option[Double], longitude: Option[Double])
+
+trait Protocols extends DefaultJsonProtocol {
+  implicit val ipInfoFormat = jsonFormat5(IpInfo.apply)
+}
+
+trait Service extends Protocols {
+  implicit val system: ActorSystem
+  implicit def executor: ExecutionContextExecutor
+  implicit val materializer: FlowMaterializer
+
+  def config: Config
+  val logger: LoggingAdapter
+
+  def fetchIpInfo(ip: String): Future[IpInfo] = {
+    val x: Future[IpInfo] = Future(IpInfo(ip, Some("asd"), Some("xzc"), None, Some(14.3)))
+    x
   }
+
+  val routes = {
+    pathPrefix("ip") {
+      (get & path(Segment)) { ip =>
+        complete {
+          fetchIpInfo(ip)
+        }
+      }
+    }
+  }
+}
+
+
+object AkkaHttpMicroservice extends App with Service {
+  override implicit val system = ActorSystem()
+  override implicit val executor = system.dispatcher
+  override implicit val materializer = ActorFlowMaterializer()
+
+  override val config = ConfigFactory.load()
+  override val logger = Logging(system, getClass)
+
+  Http().bind(interface = "0.0.0.0", port = 9000).startHandlingWith(routes)
 }
